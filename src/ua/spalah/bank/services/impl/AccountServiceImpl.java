54acf4c5ea3597c98c69846a6a6e9fd6be65b8e0
Eugene <drawnowl@gmail.com>
2017-01-06 18:19:31 +0200

    - refactoring - exceptions

--- src/ua/spalah/bank/services/impl/AccountServiceImpl.java
+++ src/ua/spalah/bank/services/impl/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package ua.spalah.bank.services.impl;
 import ua.spalah.bank.exceptions.NotEnoughFundsException;
 import ua.spalah.bank.models.Account;
 import ua.spalah.bank.models.AccountType;
+import ua.spalah.bank.models.CheckingAccount;
 import ua.spalah.bank.services.AccountService;
 
 public class AccountServiceImpl implements AccountService {
@@ -18,16 +19,19 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public void withdraw(Account account, double amount) throws NotEnoughFundsException {
-        if(amount <= account.getBalance()) {
-//            if(account.getType() == AccountType.CHECKING) {
-//                if(account.getBalance() + account.get > amount) {
-//                    account.setBalance(account.getBalance() - amount);
-//            }  else {
-            account.setBalance(account.getBalance() - amount);
-//            }
+        if (amount <= account.getBalance()) {
+            if (account.getType() == AccountType.CHECKING) {
+                CheckingAccount checkingAccount = (CheckingAccount) account;
+                if (checkingAccount.getBalance() + checkingAccount.getOverdraft() > amount) {
+                    account.setBalance(account.getBalance() - amount);
+                }
+            } else {
+                account.setBalance(account.getBalance() - amount);
+            }
         } else {
             throw new NotEnoughFundsException(account.getBalance(), amount);
         }
+
     }
 
     @Override
